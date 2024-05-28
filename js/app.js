7e8d7cf6846f695ba22d8afd73efe619797d7c19
menghorngkh <menghorng4636@gmail.com>
2024-05-28 08:35:32 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// menubar = document.querySelector('.bar');
+// lest = document.querySelector('.lest');
+
+// menubar.addEventListener('click', function(){
+//     lest.classList.toggle('active');
+// });
+
+function OpenNav(){
+    document.getElementById("Nav").style.width = "85%";
+    }
+    function CloseNav(){
+        document.getElementById("Nav").style.width = "0%";
+}
